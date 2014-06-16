5e6f95d74fee662582bf5777e337b045f8bd37ed
Whitney Young <wbyoung@fadingred.com>
2014-06-16 10:55:59 -0700

    Basic Express app.

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var express = require('express');
+var path = require('path');
+
+var run = module.exports = function(env) {
+  var app = express();
+  app.use(express.static(path.join(__dirname, 'public')));
+  return app;
+};
+
+if (require.main === module) {
+  var env = process.env.NODE_ENV || 'development';
+  var port = process.env.PORT || 3000;
+
+  run(env).listen(port, function() {
+    console.log('Express server started on port %s using %s auth', port, auth);
+  });
+}
